b0a1e66823dafecbd8feef1fafddb915437f8854
Elliot Wright <wright.elliot@gmail.com>
2026-04-21 19:30:15 +0100

    Example highlighting the issue

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,14 +3,15 @@ package dev.seeruk
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Example(val id: Int)
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello, World!")
-        }
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
+        post<Example>("/example") { example ->
+            call.respondText(example.toString())
         }
     }
 }
